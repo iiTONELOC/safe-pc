ca8c2cbb530e49292db7e81731cf0de8cfef4beb
iiTONELOC <anthonytropeano@me.com>
2025-09-25 20:53:25 +0000

    Saving Previous work.

--- capstone/proxmox/auto_installer/answer_server.py
+++ capstone/proxmox/auto_installer/answer_server.py
@@ -1,18 +1,33 @@
-import json
+from json import dumps
+from logging import INFO
 from multiprocessing import Process
-from capstone.proxmox.answer_file.utils import gen_ans_file_with
-
-from flask import Flask, request, make_response, jsonify
-import tempfile
-from flask import send_file
-
-
-app = Flask(__name__)
+from tempfile import NamedTemporaryFile
+from socket import socket, AF_INET, SOCK_DGRAM
+from capstone.utils.logs import setup_logging
+from capstone.proxmox.answer_file.answer_file import gen_ans_file_with
+from flask import Flask, request, make_response, jsonify, send_file
 
 cached_answer_files = {}
 
+app = Flask(
+    "capstone.proxmox.auto_installer.answer_server"
+    if __name__ == "__main__"
+    else __name__
+)
 
-# NOT SURE WHY BUT PROXMOX Installer is making a POST request
+
+def _get_ip() -> str:
+    s = socket(AF_INET, SOCK_DGRAM)
+    try:
+        s.connect(("10.254.254.254", 1))
+        ip = s.getsockname()
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip[0] if isinstance(ip, tuple) else ip  # type: ignore
+
+
 @app.route("/api/answer_file", methods=["POST"])
 def answer_file():
     data = request.json
@@ -22,9 +37,9 @@ def answer_file():
     if key in cached_answer_files:
         print(f"Serving cached answer file for {key}")
         # Write the answer file content to a temporary file
-        temp = tempfile.NamedTemporaryFile(delete=False, mode="w+", suffix=".toml")
+        temp = NamedTemporaryFile(delete=False, mode="w+", suffix=".toml")
         temp.write(cached_answer_files[key])  # type: ignore
-        print(f"Answer file content:\n{cached_answer_files[key]}")
+        app.logger.info(f"Answer file content:\n{cached_answer_files[key]}")
         temp.flush()
         temp.seek(0)
         response = send_file(
@@ -35,7 +50,7 @@ def answer_file():
         )
         return response
     else:
-        print(f"No cached answer file for {key}")
+        app.logger.error(f"No cached answer file for {key}")
         # use the data from the request to find the network interface and disk
         return make_response(
             jsonify({"error": "No answer file found for this client"}), 404
@@ -54,9 +69,9 @@ def device_discovery():
 
     if key not in cached_answer_files:
         cached_answer_files[key] = gen_ans_file_with(key, disk)
-        print(f"Generated new answer file for {key}")
-    print("\nDevice Discovery Data Received:")
-    print(json.dumps(data, indent=4))
+        app.logger.info(f"Generated new answer file for {key}")
+    app.logger.info("Device Discovery Data Received:")
+    app.logger.info(dumps(data, indent=4))
     return make_response(jsonify({"message": "Device discovery data received"}), 200)
 
 
@@ -67,24 +82,26 @@ def _start_server_new_process():
 
 
 def start_answer_server():
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host=_get_ip(), port=5000, debug=True)
 
 
 def main():
     server_process = None
+    app.logger.setLevel("INFO")
     try:
         server_process = _start_server_new_process()
         server_process.join()
     except KeyboardInterrupt:
-        print("Shutting down the server.")
+        app.logger.info("Shutting down the server.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        app.logger.error(f"An error occurred: {e}")
     finally:
         if server_process:
             server_process.terminate()
             server_process.join()
-        print("Server has been shut down.")
+        app.logger.info("Server has been shut down.")
 
 
 if __name__ == "__main__":
+    setup_logging(level=INFO)
     main()
